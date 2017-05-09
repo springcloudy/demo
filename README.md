32dcd83928bc8a5ff74e8bafa4dee3526f4f9aca
Yang Yang <reliveyy@gmail.com>
2017-05-09 15:50:44 +0800

    Add gateway SimpleFilter bean

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # demo
 
-* rabbitmq-server
+* rabbitmq-server: used by eureka
+* redis-server: used by dataflow-server
 * service-registry(eureka): 8761
 * config-server: 8888
 * gateway: 8080
 * dummy-service: 8081
 * admin: 8082
 * dataflow-server: 9393
-* dataflow-shell: 
+* dataflow-shell: 8083
 
 ### Web GUI:
 * service-registry: 8761/
@@ -17,3 +18,7 @@
 ### Test URLs:
 
 * http://localhost:8080/dummy/hello
+
+### Notice
+
+If config repo is empty then config-server endpoint /health will be unavailable.
--- gateway/src/main/java/com/exmaple/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/exmaple/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -13,5 +14,10 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
     }
 }
